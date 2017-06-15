d91e72149665a991c7c902e95ff211fc8aebcd1a
Laurens-Post <contact@lphosting.nl>
2017-06-16 00:28:58 +0200

    Airview API

---
+++ database/migrations/2017_04_11_095830_create_flights_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('flightnumber');
+			$table->integer('arrivalAirport_id');
+			$table->datetime('arrivalDateTime');
+			$table->integer('depatureAirport_id');
+			$table->datetime('depatureDateTime');
+			$table->string('status'); // onetime, delayed
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+}
